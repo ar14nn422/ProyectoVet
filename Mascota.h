b4e306df74a490530841aad644cf100706b6bafb
abarquero60814@ufide.ac.cr <abarquero60814@ufide.ac.cr>
2024-08-21 12:40:24 -0600

    Doctor y Mascota

--- Mascota.h
+++ Mascota.h
@@ -1,1 +1,13 @@
-
+class Macota {
+private:
+	std::string nombre;
+	char tipo;
+public:
+	Mascota();
+	Mascota(std::string, char);
+	void setNombre(std::string);
+	std::string getNombre();
+	void setTipo(char);
+	std::string getTipo();
+	std::string mostrarMascota();
+};
