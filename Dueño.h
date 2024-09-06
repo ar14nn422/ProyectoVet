0f4455bd6261368eb4c8911598bff68425215d1c
Arianna <87552392+ar14nn422@users.noreply.github.com>
2024-09-05 21:38:51 -0600

    Update Dueño.h

--- Dueño.h
+++ Dueño.h
@@ -1,1 +1,22 @@
 
+#include "coleccionmascotas.h"
+#ifndef DUEÑO_H
+#define DUEÑO_H
+class Dueño {
+public:
+	string setNombreDuenno;
+	coleccionmascotas* mascotas;
+private:
+	Dueño();
+	Dueño(string, coleccionmascotas*);
+	void setNombreDuenno(string);
+	void setColeccionmascotas(coleccionmascotas*);
+	string getNombreDuenno();
+	coleccionmascotas* getMascotas();
+	void asignarMascotas();
+	string mostrarMasc_d();
+
+
+
+};
+#endif // !DUEÑO_H
