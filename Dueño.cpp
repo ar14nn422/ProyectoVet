904115b200a368f64b0cd550e32906d76134983a
Arianna <87552392+ar14nn422@users.noreply.github.com>
2024-09-05 21:37:29 -0600

    Update Dueño.cpp

--- Dueño.cpp
+++ Dueño.cpp
@@ -1,1 +1,36 @@
+#include "Dueño.h"
+Dueño::Dueño() {
 
+}
+Dueño::Dueño(string, coleccionmascotas*) {
+
+}
+void Dueño::setNombreDuenno(string)
+{
+}
+
+void Dueño::setColeccionmascotas(coleccionmascotas*)
+{
+}
+
+string Dueño::getNombreDuenno()
+{
+	return string();
+}
+
+coleccionmascotas* Dueño::getMascotas()
+{
+	return nullptr;
+}
+
+void Dueño::asignarMascotas()
+{
+	mascota* nuevaMascota = new coleccionmascotas();
+	mascota->agregarMascota(nuevaMascota);
+
+}
+
+string Dueño::mostrarMasc_d()
+{
+	return string();
+}
